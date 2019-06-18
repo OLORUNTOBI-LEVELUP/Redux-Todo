f62b28b078e4cf8d21733df5da863fb71a2ae0c0
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-18 13:31:29 +0100

    Created my Action types

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+
+export const DELETE_TODO = 'DELETE_TODO';
+export const ADD_TODO = 'ADD_TODO';
+export const MARK_COMPLETED = 'MARK_COMPLETED';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
