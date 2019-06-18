47855cd7969ccba1af518e6fe0c643538482a6bb
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-18 13:56:43 +0100

    Created reducers

--- todo/src/actions/actions.js
+++ todo/src/actions/actions.js
@@ -1,8 +1,8 @@
 export const ADD_TASK = "ADD_TASK"
 export const TOGGLE_TASK = "TOGGLE_TASK"
 
-export const addTask = input => {
-  return { type: ADD_TASK, input }
+export const addTask = text => {
+  return { type: ADD_TASK, text }
 }
 
 export const toggleTask = id => {
---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,25 @@
+import { ADD_TASK, TOGGLE_TASK } from "../actions/actions"
+
+// const initialSate = {
+//   todos: []
+// }
+
+const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false,
+          id: action.id
+        }
+      ]
+    case TOGGLE_TASK:
+      return state.map(task =>
+        task.id === action.id ? { ...task, completed: !task.completed } : task
+      )
+    default:
+      return state
+  }
+}
